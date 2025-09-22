ad891b727a517ec0dafd709f81f8569f3456ec78
altmanLong <273135138@qq.com>
2025-09-22 12:02:41 +0800

    switch url

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# # 设置环境变量
+# export LIGHTER_ACCOUNT_INDEX=102110
+# export LIGHTER_API_KEY_INDEX=2
+# export LIGHTER_API_KEY_PRIVATE_KEY="你需要在这里填入你的私钥"
+
+# 激活虚拟环境
+source venv/bin/activate
+
+# 运行程序
+/opt/homebrew/bin/python3 main.py
